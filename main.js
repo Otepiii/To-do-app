5c54ea437b4327fbe8d8af8c1abf36478f7b6264
joseph Evangelista <oootep123@gmail.com>
2020-06-11 22:52:30 -0400

    finalized modification and js file

--- main.js
+++ main.js
@@ -6,8 +6,38 @@ const input = document.getElementById("input");
 
 // Classes names
 const CHECK = "fa-check-circle";
-const UNCHECK = "fa-circle-thin";
+const UNCHECK = "fa-circle";
 const LINE_THROUGH = "lineThrough";
+
+// Variables
+let LIST, id;
+
+// get item from localstorage
+let data = localStorage.getItem("TODO");
+
+// check if data is not empty
+if (data) {
+  LIST = JSON.parse(data);
+  id = LIST.length; // set the id to the last one in the list
+  loadList(LIST); // load the list to the user interface
+} else {
+  // if data is not empty
+  LIST = [];
+  id = 0;
+}
+
+// load items to the users interface
+function loadList(array) {
+  array.forEach(function (item) {
+    addToDo(item.name, item.id, item.done, item.trash);
+  });
+}
+
+// Clear the localstorage
+clear.addEventListener("click", function () {
+  localStorage.clear();
+  location.reload();
+});
 
 // Show todays date
 const today = new Date();
@@ -26,9 +56,9 @@ function addToDo(toDo, id, done, trash) {
   const LINE = done ? LINE_THROUGH : "";
 
   const item = `<li class="item">
-                  <i class="fas  ${DONE}  co" job="complete" id="${id}"></i>
+                  <i class="far  ${DONE}  co" job="complete" id="${id}"></i>
                   <p class="text ${LINE}" > ${toDo} </p>
-                  <i class="fas fa-trash-o de" job="delete" id="${id}"></i>
+                  <i class="fas fa-trash de" job="delete" id="${id}"></i>
                 </li>`;
   const position = "beforeend";
 
@@ -42,13 +72,57 @@ document.addEventListener("keyup", function (event) {
 
     // if the input isnt empty
     if (toDo) {
-      addToDo(toDo);
+      addToDo(toDo, id, false, false);
+
+      LIST.push({
+        name: toDo,
+        id: id,
+        done: false,
+        trash: false,
+      });
+
+      // add item to localstorage (this code must be added where the LIST array is updated)
+      localStorage.setItem("TODO", JSON.stringify(LIST));
+
+      id++;
     }
     input.value = "";
   }
 });
 
-addToDo("Coffee", 1, true, false);
+// addToDo("Coffee", 1, true, false);
+
+// Completes to do
+function completeToDo(element) {
+  element.classList.toggle(CHECK);
+  element.classList.toggle(UNCHECK);
+  element.parentNode.querySelector(".text").classList.toggle(LINE_THROUGH);
+
+  LIST[element.id].done = LIST[element.id].done ? false : true;
+}
+
+// remove to do
+function removeToDo(element) {
+  element.parentNode.parentNode.removeChild(element.parentNode);
+
+  LIST[element.id].trash = true;
+}
+
+// target the items created dynamically
+
+list.addEventListener("click", function (event) {
+  const element = event.target; // return the clicked element inside list
+  const elementJob = element.attributes.job.value; // complete or delete
+
+  if (elementJob == "complete") {
+    completeToDo(element);
+  } else if (elementJob == "delete") {
+    removeToDo(element);
+  }
+
+  // add item to localstorage (this code must be added where the LIST array is updated)
+  localStorage.setItem("TODO", JSON.stringify(LIST));
+});
 
 // list.insertAdjacentHTML("beforeend", text);
 
